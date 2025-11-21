f547dd897ef12e656c2b739b1d0cdaba7708c0de
McLaren95 <159812204+McLaren95@users.noreply.github.com>
2025-11-21 20:22:07 +0300

    Добавлены модели Airplane и Airport, проверены таблицы в БД

--- airline_app/admin.py
+++ airline_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Airplane, Airport
 
-# Register your models here.
+admin.site.register(Airplane)
+admin.site.register(Airport)
--- airline_app/models.py
+++ airline_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Airplane(models.Model):
+    airplane_code = models.CharField(max_length=10, primary_key=True)
+    model = models.JSONField()
+    range = models.PositiveIntegerField()
+    speed = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.airplane_code} ({self.model.get('en', '')})"
+
+    class Meta:
+        db_table = 'airplanes_data'
+
+class Airport(models.Model):
+    airport_code = models.CharField(max_length=3, primary_key=True)
+    airport_name = models.JSONField()
+    city = models.JSONField()
+    country = models.JSONField()
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    timezone = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.airport_code} ({self.airport_name.get('en', '')})"
+
+    class Meta:
+        db_table = 'airports_data'
